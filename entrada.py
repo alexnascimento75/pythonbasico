a0e548509f94fc9783786335f493ec05d8064453
Alex Nascimento <79993335+alexnascimento75@users.noreply.github.com>
2024-10-09 12:17:50 -0300

    Add files via upload
    Entrada e tipos primitivos

---
+++ entrada.py
@@ -0,0 +1,12 @@
+dado = input('Digite a sua idade: ')
+print('A idade digitada foi: ', dado)
+dado = input('Digite seu nome: ')
+print('Meu nome é:', dado)
+dado = input('Digite seu telefone' )
+print('Meu telefone é:', dado)
+dado = input('Digite seu endereço: ')
+print('Meu endereço é:', dado)
+
+# Dados de entrada e saída sem o usio de variáveis
+
+                
