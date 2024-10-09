a9aa755d21c5a601ffb013ed1c3556cc48046c4b
Alex Nascimento <79993335+alexnascimento75@users.noreply.github.com>
2024-10-09 09:26:23 -0300

    Add files via upload
    Comandos básicos em python visando estudos

---
+++ saida.py
@@ -0,0 +1,11 @@
+print('Olá, seja bem vindo!')
+
+print(f'O valor inteiro em decimal é: {10: d}')
+print(f'O valor inteiro em binário é: {10: b}')
+print(f'O valor de pi é: {3.14159265: f}') # O float mantém apenas sete algarismo com precisão
+print(f'O valor de pi é: {3.14159265: .2f}') # Truncado para duas casas decimais
+
+print('Nome: Alexander Abidias do Nascimento')
+print(f'Idade: {49} anos')
+print(f'Salário:{3875.4657: .2f}')
+print('Nacionalidade: brasileiro')
