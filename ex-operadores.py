23dd59dad6e1118cb8aea284043912f91d624001
Alex Nascimento <79993335+alexnascimento75@users.noreply.github.com>
2024-10-10 11:26:17 -0300

    Add files via upload
    Operações aritméticas

---
+++ ex-operadores.py
@@ -0,0 +1,25 @@
+# Criar uma variável inteira e atribua um valor arbitrário e ela, de sua prferência.
+# Imprima o conteúdo. Em seguida, utilizar os operadores de atribuição aritmética para:
+# Incrementar o conteúdo da variável em 1 unidade e imprimir na tela;
+# Decrementar o conteúdo da variável em 1 unidade e imprimir na tela; 
+
+dado = 10
+print(f'Conteúdo da variável dado: , {dado}')
+
+dado += 1 # dado = dado + 1
+print(f'Conteúdo da variável dado, depois do incremento: {dado}')
+
+dado -= 1 # dado = dado - 1
+print(f'Conteúdo da variável dado, depois do decremento: {dado}')
+
+# Dupliocar o conteúdo da variável em memória, atualizar o conteúdo da variável, e imprimir o resultado;
+# Calcular a metade da variável em memória, atualizar o conteúdo da variável, e imprimir o resultado;
+
+dado *= 2 # dado = dado * 2
+print(f'Conteúdo da variável dado, depois do incremento: {dado}')
+
+dado /= 2 # dado = dado / 2
+print(f'Conteúdo da variável dado, depois do decremento: {dado}')
+
+
+
