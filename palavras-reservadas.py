cde763779eae77c80c29da5bf62fe7113a786a36
Alex Nascimento <79993335+alexnascimento75@users.noreply.github.com>
2024-10-17 16:22:55 -0300

    Add files via upload

---
+++ palavras-reservadas.py
@@ -0,0 +1,11 @@
+# Criando a variável flag, do tipo bool ( recebendo true)
+flag =  True
+
+# Na sequência, estamos imprimidmo o conteúdo flag na tela
+print('O valor da variável booleano é: ', flag)
+
+''' 
+Este é um teste
+de comentário 
+várias linhas 
+'''
